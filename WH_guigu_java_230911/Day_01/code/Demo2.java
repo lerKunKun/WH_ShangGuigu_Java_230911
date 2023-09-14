232aff6109b9789310d88cd9465e3613a3de8e1d
lerKunKun <72156778+lerKunKun@users.noreply.github.com>
2023-09-14 15:27:18 +0800

    Add files via upload

---
+++ WH_guigu_java_230911/Day_01/code/Demo2.java
@@ -0,0 +1,11 @@
+public class Demo2{
+	public static void main(String[] args){
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+}
---
+++ WH_guigu_java_230911/Day_01/code/Demo3.java
@@ -0,0 +1,14 @@
+//定义类 [单行注释]
+public class Demo3{
+	/*
+		编写主方法 : 
+			主方法程序的入口,没有主方法程序无法运行
+	*/
+	public static void main(String[] args){
+		/**
+			这里是文档注释/文本注释
+			编写输出语句 -> 可以把输出语句括号内的内容输出在控制台
+		*/
+		System.out.println("谈斌");
+	}
+}
