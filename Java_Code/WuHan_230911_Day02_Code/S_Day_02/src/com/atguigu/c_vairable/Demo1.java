a84720877197eeb690c1c2f76c82befe496df786
lerKunKun <72156778+lerKunKun@users.noreply.github.com>
2023-10-11 13:50:10 +0800

    Delete Java_Code directory

--- Java_Code/WuHan_230911_Day02_Code/S_Day_02/src/com/atguigu/c_vairable/Demo1.java
+++
@@ -1,9 +0,0 @@
-package com.atguigu.c_vairable;
-
-public class Demo1 {
-    public static void main(String[] args) {
-        byte num = 100;
-        int b = num;//小转大,自动提升
-        System.out.println(b);
-    }
-}
--- Java_Code/WuHan_230911_Day02_Code/S_Day_02/src/com/atguigu/c_vairable/Demo2.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.c_vairable;
-
-public class Demo2 {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
